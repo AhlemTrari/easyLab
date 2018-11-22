d3184d8212af683d6af547fde1626816293d0e99
Ahlem Trari <trari.ahlem@gmail.com>
2018-11-22 22:47:15 +0100

    ajout de la page contact

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,10 @@ Route::get('/frontOffice/membres', function () {
     return view('frontOffice/membres');
 });
 
+Route::get('/frontOffice/contact', function () {
+    return view('frontOffice/contact');
+});
+
 
 
 	// backoffice
