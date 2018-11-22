220fb4ee266e23455bf4c2d5915bc4bb8173619e
Feriel BRIKCI <ferielbrikci96@gmail.com>
2018-11-22 13:18:48 +0100

    ajout frontOffice directory
    ajout equipe

---
+++ resources/views/frontOffice/equipes.blade.php
@@ -0,0 +1,289 @@
+
+	<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="img/fav.png">
+		<!-- Author Meta -->
+		<meta name="author" content="colorlib">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>Lrit</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="css/linearicons.css">
+			<link rel="stylesheet" href="css/font-awesome.min.css">
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/magnific-popup.css">
+			<link rel="stylesheet" href="css/nice-select.css">							
+			<link rel="stylesheet" href="css/animate.min.css">
+			<link rel="stylesheet" href="css/owl.carousel.css">			
+			<link rel="stylesheet" href="css/jquery-ui.css">			
+			<link rel="stylesheet" href="css/main.css">
+		</head>
+		<body>	
+		  <header id="header" id="home">
+	  		<div class="header-top">
+	  			<div class="container">
+			  		<div class="row">
+			  			<div class="col-lg-6 col-sm-6 col-8 header-top-left no-padding">
+			  				<ul>
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+								<li><a href="#"><i class="fa fa-behance"></i></a></li>
+			  				</ul>			
+			  			</div>
+			  			<div class="col-lg-6 col-sm-6 col-4 header-top-right no-padding">
+			  				<a href="tel:+953 012 3654 896"><span class="lnr lnr-phone-handset"></span> <span class="text">+953 012 3654 896</span></a>
+			  				<a href="mailto:support@colorlib.com"><span class="lnr lnr-envelope"></span> <span class="text">support@colorlib.com</span></a>			
+			  			</div>
+			  		</div>			  					
+	  			</div>
+			</div>
+		    <div class="container main-menu">
+		    	<div class="row align-items-center justify-content-between d-flex">
+			      <div id="logo">
+			        <a href="index.html"><img src="#" alt="" title="" /></a>
+			      </div>
+			      <nav id="nav-menu-container">
+			        <ul class="nav-menu">
+			          <li><a href="index.html">Accueil</a></li>
+			          <li><a href="about.html">A propos</a></li>
+			          <li><a href="equipes.html">Equipes</a></li>
+			          <li><a href="events.html">Membres</a></li>
+			          <li><a href="gallery.html">Projets</a></li>
+			          
+			          		          					          		          
+			          <li><a href="contact.html">Contact</a></li>
+			        </ul>
+			      </nav><!-- #nav-menu-container -->		    		
+		    	</div>
+		    </div>
+		  </header><!-- #header -->
+		  <!-- start banner Area -->
+			<section class="banner-area relative about-banner" id="home">	
+				<div class="overlay overlay-bg"></div>
+				<div class="container">				
+					<div class="row d-flex align-items-center justify-content-center">
+						<div class="about-content col-lg-12">
+							<h1 class="text-white">
+								Nos equipes		
+							</h1>	
+							<p class="text-white link-nav"><a href="index.html">Accueil </a>  <span class="lnr lnr-arrow-right"></span>  <a href="equipes.html"> Equipes</a></p>
+						</div>	
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->
+			
+			<!--contenu-->
+			<section class="course-details-area ">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12 left-contents">
+							<div class="jq-tab-wrapper" id="horizontalTab">
+	                            <div class="jq-tab-menu">
+	                                <div class="jq-tab-title active" data-tab="1">Présentation</div>
+	                                <div class="jq-tab-title" data-tab="2">Equipes de recherche</div>
+	                                
+	                            </div>
+	                            <div class="jq-tab-content-wrapper">
+	                                <div class="jq-tab-content active" data-tab="1">
+	                                    
+	                                    	<div class="container">
+
+
+	                                    	<div class="row">
+	                                    			<p>
+	                                    		Le LRI, Laboratoire Mixte de Recherche de l'Université de Tlemcen, est un laboratoire de recherche en informatique se consacrant à la modélisation et la résolution de problèmes fondamentaux motivés par les applications, ainsi qu'à la mise en oeuvre et la validation des solutions au travers de partenariats académiques comme le département de Mathématique et de Physique Electronique.
+												Les axes fédérateurs sont :
+												Sûreté, sécurité, fiabilité ;
+												Science des données, intelligence et optimisation ; 
+												Systèmes communicants.
+												Le LRI répond à ces challenges à différents niveaux thématiques au sein des 3 départements.
+												 
+									
+											</div>
+											<div class="row col-md-12">
+												<div class="feature-img">
+												<img class="img-fluid" src="img/lab.jpg" alt="">
+												</div>
+											</div>
+	                                    	
+	                                    
+	                                    
+	                                   
+                     				 </div>
+	                                </div>
+	                                <div class="jq-tab-content" data-tab="2">
+	                                   <section class="upcoming-event-area section-gap">
+											<div class="container">					
+												<div class="row">
+													<div class="col-12 col-md-2 thumb">
+													<img class="img-fluid" src="img/s.png" alt="">
+													</div>
+													<div class="detials col-12 col-md-4">
+													
+													<a href="detailEquipes.html"><h2>SIDK</h2></a>
+													<p><h4>Système d'information et de connaissance</h4></p>
+													</div>
+													<div class="col-12 col-md-2 thumb">
+													<img class="img-fluid" src="img/c.jpg" alt="">
+													</div>
+													<div class="detials col-12 col-md-4">
+													
+													<a href="detailEquipes.html"><h2>ILS</h2></a>
+													<p><h4>Ingénierie logicielle sécurisée</h4></p>
+													</div>
+												</div>
+												<div class="row"><p></p></div>
+												
+												<div class="row">
+													<div class="col-12 col-md-2 thumb">
+													<img class="img-fluid" src="img/RSD.png" alt="">
+													</div>
+													<div class="detials col-12 col-md-4">
+													<p></p>
+													<a href="detailEquipes.html"><h2>RSDS</h2></a>
+													<p><h4>Réseau, service distribués et systèmes</h4></p>
+													</div>
+													<div class="col-12 col-md-2 thumb">
+													<img class="img-fluid" src="img/IA.jpg" alt="">
+													</div>
+													<div class="detials col-12 col-md-4">
+													
+													<a href="detailEquipes.html"><h2>IA</h2></a>
+													<p><h4>Intelligence artificielle</h4></p>
+													</div>
+																												
+												</div>
+												<div class="row"><p></p></div>
+
+												
+											</div>
+										
+										
+										
+	                                </div>
+
+	                            </div>
+	                        </div>
+						</div>
+						
+					</div>
+				</div>	
+			</section>
+			<!--fin contenu-->
+					
+			<!-- start footer Area -->		
+			<footer class="footer-area section-gap">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-2 col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h4>Top Products</h4>
+								<ul>
+									<li><a href="#">Managed Website</a></li>
+									<li><a href="#">Manage Reputation</a></li>
+									<li><a href="#">Power Tools</a></li>
+									<li><a href="#">Marketing Service</a></li>
+								</ul>								
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h4>Quick links</h4>
+								<ul>
+									<li><a href="#">Jobs</a></li>
+									<li><a href="#">Brand Assets</a></li>
+									<li><a href="#">Investor Relations</a></li>
+									<li><a href="#">Terms of Service</a></li>
+								</ul>								
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h4>Features</h4>
+								<ul>
+									<li><a href="#">Jobs</a></li>
+									<li><a href="#">Brand Assets</a></li>
+									<li><a href="#">Investor Relations</a></li>
+									<li><a href="#">Terms of Service</a></li>
+								</ul>								
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h4>Resources</h4>
+								<ul>
+									<li><a href="#">Guides</a></li>
+									<li><a href="#">Research</a></li>
+									<li><a href="#">Experts</a></li>
+									<li><a href="#">Agencies</a></li>
+								</ul>								
+							</div>
+						</div>																		
+						<div class="col-lg-4  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h4>Newsletter</h4>
+								<p>Stay update with our latest</p>
+								<div class="" id="mc_embed_signup">
+									 <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get">
+									  <div class="input-group">
+									    <input type="text" class="form-control" name="EMAIL" placeholder="Enter Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email Address '" required="" type="email">
+									    <div class="input-group-btn">
+									      <button class="btn btn-default" type="submit">
+									        <span class="lnr lnr-arrow-right"></span>
+									      </button>    
+									    </div>
+									    	<div class="info"></div>  
+									  </div>
+									</form> 
+								</div>
+							</div>
+						</div>											
+					</div>
+					<div class="footer-bottom row align-items-center justify-content-between">
+						<p class="footer-text m-0 col-lg-6 col-md-12"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+						<div class="col-lg-6 col-sm-12 footer-social">
+							<a href="#"><i class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-twitter"></i></a>
+							<a href="#"><i class="fa fa-dribbble"></i></a>
+							<a href="#"><i class="fa fa-behance"></i></a>
+						</div>
+					</div>						
+				</div>
+			</footer>	
+			<!-- End footer Area -->	
+
+
+			<script src="js/vendor/jquery-2.2.4.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="js/vendor/bootstrap.min.js"></script>			
+			<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+  			<script src="js/easing.min.js"></script>			
+			<script src="js/hoverIntent.js"></script>
+			<script src="js/superfish.min.js"></script>	
+			<script src="js/jquery.ajaxchimp.min.js"></script>
+			<script src="js/jquery.magnific-popup.min.js"></script>	
+    		<script src="js/jquery.tabs.min.js"></script>						
+			<script src="js/jquery.nice-select.min.js"></script>	
+			<script src="js/owl.carousel.min.js"></script>									
+			<script src="js/mail-script.js"></script>	
+			<script src="js/main.js"></script>	
+		</body>
+	</html>
